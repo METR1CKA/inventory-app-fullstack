b0fe0b7113db83d0a9de97960d2064fecd08089d
METR1CKA <ferchosalazar054@gmail.com>
2024-12-04 17:43:00 -0600

    feat: home test added

--- tests/functional/home.spec.ts
+++ tests/functional/home.spec.ts
@@ -4,18 +4,18 @@ import User from 'App/Models/User'
 
 test.group('Pagina principal', () => {
     test('Debe de devolver la pagina principal', async ({ client }) => {
+        const user = await User.findByOrFail('email', Env.get('EMAIL'))
+
         const responseLogin = await client
             .post('/login')
             .form({
-                email: Env.get('EMAIL'),
-                password: Env.get('PASSWORD'),
+                email: user.email,
+                password: user.password,
             })
             .withCsrfToken()
 
         responseLogin.assertStatus(200)
         responseLogin.assertRedirectsToRoute('/')
-
-        const user = await User.findByOrFail('email', Env.get('EMAIL'))
 
         const responseHome = await client.get('/').loginAs(user)
 
