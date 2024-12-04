f6ee4ee3a81f7110ef4221b4568330a8c87285ae
METR1CKA <ferchosalazar054@gmail.com>
2024-12-04 17:36:31 -0600

    feat: modules added

--- app/Routes/modules.ts
+++ app/Routes/modules.ts
@@ -1,5 +1,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+async function getHome({ view }) {
+    return view.render('home')
+}
+
+async function removeToast({ params, session, response }) {
+    if (params.key && session.has(params.key)) session.forget(params.key)
+    return response.noContent()
+}
+
 Route.group(() => {
     Route.group(() => {
         Route.resource('users', 'UsersController').as('users')
@@ -9,10 +18,7 @@ Route.group(() => {
         Route.resource('products', 'ProductsController').as('products')
     }).namespace('App/Controllers/Http/Modules')
 
-    Route.get('/', async ({ view }) => await view.render('home')).as('/')
+    Route.get('/', getHome).as('/')
 
-    Route.post('forget-toast/:key', async ({ params, session, response }) => {
-        if (params.key && session.has(params.key)) session.forget(params.key)
-        return response.noContent()
-    }).prefix('api/v1')
+    Route.post('forget-toast/:key', removeToast).prefix('api/v1')
 }).middleware('auth:web')
